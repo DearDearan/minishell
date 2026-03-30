60d0104d671cee835e295bd273e52fb021825f2c
Liam Francois <lifranco@2C1.42angouleme.fr>
2026-03-30 16:44:57 +0200

    feat: beginned the parsing, made a new struct called s_cmd.

--- include/parsing.h
+++ include/parsing.h
@@ -6,27 +6,29 @@
 /*   By: lifranco <lifranco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 16:25:21 by lifranco          #+#    #+#             */
-/*   Updated: 2026/03/31 16:14:34 by lifranco         ###   ########.fr       */
+/*   Updated: 2026/03/31 16:17:00 by lifranco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef	PARSING_H
 # define PARSING_H 
 
-enum redirect
+typedef enum e_type
 {
-	RED_IN,
-	RED_OUT,
-	RED_LIM,
-	RED_APP
-};
+	WORDS,
+	FILENAME,
+	IN,
+	OUT,
+	LIM,
+	APP
+} t_type;
 
-typedef struct s_cmd
+typedef struct s_lexer
 {
-	char	**cmd;
-	int		red_type;
-	char	*filename;
-	struct s_cmd	*next;
-}	t_cmd;
+	char	*content;
+	int		type;
+	t_lexer	*next;
+
+}	t_lexer;
 
 #endif
--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,11 +6,11 @@
 /*   By: lifranco <lifranco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 15:12:38 by lifranco          #+#    #+#             */
-/*   Updated: 2026/03/30 18:26:58 by lifranco         ###   ########.fr       */
+/*   Updated: 2026/03/31 16:15:49 by lifranco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../include/minishell.h"
+#include "minishell.h"
 
 t_cmd *parse(int argc, char **argv, char **envp)
 {
