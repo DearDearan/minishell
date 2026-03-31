b6daed9441f65abb4ed34d2c4640912cf55af641
Liam Francois <lifranco@2C2.42angouleme.fr>
2026-03-31 16:08:25 +0200

    feat: To change, initialized cmd to -1 for fds unutilized

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,11 @@
 /*   By: lifranco <lifranco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 14:24:39 by lifranco          #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2026/03/31 16:14:14 by lifranco         ###   ########.fr       */
+=======
+/*   Updated: 2026/03/31 13:45:15 by lifranco         ###   ########.fr       */
+>>>>>>> 85ebb81 (feat: To change, initialized cmd to -1 for fds unutilized)
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +18,16 @@
 
 int	main(int argc, char **argv, char **envp)
 {
+<<<<<<< HEAD
 	(void)argc;
 	(void)argv;
 	(void)envp;
 	return 0;
+=======
+ (void)argc;
+ (void)argv;
+ (void)envp;
+ return 0;
+>>>>>>> 85ebb81 (feat: To change, initialized cmd to -1 for fds unutilized)
 }
 
--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,18 +6,30 @@
 /*   By: lifranco <lifranco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 15:12:38 by lifranco          #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2026/03/31 16:19:47 by lifranco         ###   ########.fr       */
+=======
+/*   Updated: 2026/03/31 14:17:51 by lifranco         ###   ########.fr       */
+>>>>>>> 85ebb81 (feat: To change, initialized cmd to -1 for fds unutilized)
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+<<<<<<< HEAD
 t_cmd *parse(int argc, char **argv, char **envp)
+=======
+
+t_cmd *parse(char *line, t_lexer)
+>>>>>>> 85ebb81 (feat: To change, initialized cmd to -1 for fds unutilized)
 {
 	int 	i;
 	char	**cmds;
     t_cmd	*parsing;
 
+	parsing.fds[0] = -1;
+	parsing.fds[1] = -1;
+	parsing.pid = -1;
 	i = 0;
 	parsing = ft_calloc(argc, sizeof(t_cmd));
 	cmds = ft_calloc(sizeof(char *), argc);
