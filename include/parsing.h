5ca57a5270ef68996a754d17caff31c039cf67bb
Liam Francois <lifranco@2C2.42angouleme.fr>
2026-03-31 16:06:00 +0200

    rename: Renamed the delimiters in the enum to be shorter

--- include/parsing.h
+++ include/parsing.h
@@ -6,12 +6,17 @@
 /*   By: lifranco <lifranco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 16:25:21 by lifranco          #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2026/03/31 16:18:20 by lifranco         ###   ########.fr       */
+=======
+/*   Updated: 2026/03/31 14:32:22 by lifranco         ###   ########.fr       */
+>>>>>>> 205b94e (rename: Renamed the delimiters in the enum to be shorter)
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef	PARSING_H
 # define PARSING_H 
+
 
 typedef enum e_type
 {
@@ -28,6 +33,10 @@ typedef struct s_lexer
 	char	*content;
 	int		type;
 	t_lexer	*next;
+<<<<<<< HEAD
+=======
+
+>>>>>>> 205b94e (rename: Renamed the delimiters in the enum to be shorter)
 }	t_lexer;
 
 #endif
