1af4ee33fdefa07faf884fc6fe2271003ac812db
Liam Francois <lifranco@1F5.42angouleme.fr>
2026-04-02 10:52:45 +0200

    change: added struct behind t_cmd for linked list of t_cmd

--- include/command.h
+++ include/command.h
@@ -6,7 +6,7 @@
 /*   By: lifranco <lifranco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 10:24:31 by lifranco          #+#    #+#             */
-/*   Updated: 2026/03/31 16:20:08 by lifranco         ###   ########.fr       */
+/*   Updated: 2026/04/01 15:59:51 by lifranco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 
 typedef struct s_cmd
 {
-	char	*path;
-	char	**argv;
-	int		fds[2];
-	pid_t	pid;
-	t_cmd	*next;
+	char			*path;
+	char			**argv;
+	int				fds[2];
+	pid_t			pid;
+	struct t_cmd	*next;
 } t_cmd;
 
 #endif
