657ab3b3be27e2491e81c640612ebc11d426d8db
Liam Francois <lifranco@2A4.42angouleme.fr>
2026-04-16 13:34:09 +0200

    modified:   src/built_in/ft_pwd.c 	new file:   src/built_in/get_envp.c 	modified:   src/minishell.c

---
+++ src/built_in/get_envp.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_envp.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lifranco <lifranco@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/14 13:44:05 by lifranco          #+#    #+#             */
+/*   Updated: 2026/04/16 13:23:30 by lifranco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int	get_tab_size(char **str)
+{
+	int	i;
+
+	if (!str || !*str)
+		return (-1);
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	get_envp(char **envp, t_minishell *shell)
+{
+	int	i;
+	char **env;
+
+	i = 0;
+	env = ft_calloc(sizeof(char *), get_tab_size(envp) + 1);
+	if (!env || !*env)
+		error_exit(shell, shell->nb_cmds);
+	while (envp[i])
+	{
+		env[i] = ft_strdup(envp[i]);
+		if (!env[i])
+			error_exit(shell, shell->nb_cmds);
+		i++;
+	}
+	env[i] = NULL;
+	shell->envp = env;
+}
+
