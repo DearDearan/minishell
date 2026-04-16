85698369599246d56c99f7fe6e664b49a6403fe2
Liam Francois <lifranco@2A4.42angouleme.fr>
2026-04-16 18:32:22 +0200

    FEAT!: Added get_envp, set envp and now working on unset.

--- src/built_in/set_envp.c
+++ src/built_in/set_envp.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_envp.c                                         :+:      :+:    :+:   */
+/*   set_envp.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lifranco <lifranco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/14 13:44:05 by lifranco          #+#    #+#             */
-/*   Updated: 2026/04/16 17:35:20 by lifranco         ###   ########.fr       */
+/*   Created: 2026/04/16 15:16:55 by lifranco          #+#    #+#             */
+/*   Updated: 2026/04/16 17:29:37 by lifranco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,13 @@ static int	get_tab_size(char **str)
 	return (i);
 }
 
-void	get_envp(char **envp, t_minishell *shell)
+static void	put_into_env(char *var, char **envp, t_minishell *shell)
 {
 	int	i;
 	char **env;
 
 	i = 0;
-	env = ft_calloc(sizeof(char *), get_tab_size(envp) + 1);
+	env = ft_calloc(sizeof(char *), get_tab_size(envp) + 2);
 	if (!env || !*env)
 		error_exit(shell, shell->nb_cmds);
 	while (envp[i])
@@ -40,7 +40,34 @@ void	get_envp(char **envp, t_minishell *shell)
 			error_exit(shell, shell->nb_cmds);
 		i++;
 	}
-	env[i] = NULL;
+	env[i] = ft_strdup(var);
+	env[i + 1] = NULL;
+	if (!env)
+		error_exit(shell, shell->nb_cmds);
+	ft_free_strs(shell->envp);
 	shell->envp = env;
 }
 
+void	set_envp(t_minishell *sh, char **env, char *var)
+{
+	int		i;
+	char	*equal;
+	int		j;
+
+	i = 0;
+	j = 0;
+	equal = ft_strjoin(get_var_name(var), "=");
+	while (env && env[i])
+	{
+		if (!ft_strncmp(equal, env[i], ft_strlen(equal)))
+		{
+			free(env[i]);
+			env[i] = ft_strdup(var);
+			if (!env[i])
+				error_exit(sh, sh->nb_cmds);
+		}
+		i++;
+	}
+	if (!env[i])
+		put_into_env(var, env, sh);
+}
