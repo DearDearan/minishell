a02a739b39d49d3a8d04133dd8d4af320c35da44
Liam Francois <lifranco@1D5.42angouleme.fr>
2026-04-28 14:03:06 +0200

    change: got the right file normally.

--- src/exec/redirections.c
+++ src/exec/redirections.c
@@ -6,7 +6,7 @@
 /*   By: lifranco <lifranco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/31 14:49:58 by Camille           #+#    #+#             */
-/*   Updated: 2026/04/27 14:26:42 by lifranco         ###   ########.fr       */
+/*   Updated: 2026/04/27 16:34:53 by Camille          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,17 +61,25 @@ static int	get_fd_heredoc(t_minishell *sh, char *limiter, int size)
 {
 	int		fds[2];
 	char	*s;
+	int		line_nb;
 
 	if (pipe(fds) == -1)
 		error_exit(sh, sh->nb_cmds);
 	s = NULL;
+	line_nb = 1;
 	while (1)
 	{
 		s = readline("> ");
-		if (!s || ((int)ft_strlen(s) == size && !ft_strncmp(s, limiter, size)))
+		if (!s)
+		{
+			printf(WARN_EOF, "minishell: warning: here-document", line_nb, limiter);
+			break ;
+		}
+		if (((int)ft_strlen(s) == size && !ft_strncmp(s, limiter, size)))
 			break ;
 		ft_dprintf(fds[1], "%s\n", s);
 		free(s);
+		line_nb++;
 	}
 	free(s);
 	close(fds[1]);
