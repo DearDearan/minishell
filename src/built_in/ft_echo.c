d163f9478e77b86be4dd4b61ba9c9b0bf1c4be73
Camille Boucher <camilleboucher@users.noreply.github.com>
2026-04-29 15:03:09 +0200

    fix: correcting echo and protecting env_path

--- src/built_in/ft_echo.c
+++ src/built_in/ft_echo.c
@@ -12,59 +12,52 @@
 
 #include "minishell.h"
 
-static bool	get_option_n(int argc, char **argv);
-static void	print_echo_argv(int argc, char **argv, bool option_n);
+static int	get_last_argv_i_option_n(char **argv);
+static void	print_echo_argv(int argc, char **argv, int argv_i);
 
 int	ft_echo(t_minishell *sh, t_cmd *cmd)
 {
-	int		argc;
-	bool	option_n;
+	int	argc;
+	int	last_argv_i_option_n;
 
 	(void)sh;
 	argc = get_argc(cmd->argv);
-	option_n = get_option_n(argc, cmd->argv);
-	print_echo_argv(argc, cmd->argv, option_n);
-	if (!option_n)
+	last_argv_i_option_n = get_last_argv_i_option_n(cmd->argv);
+	print_echo_argv(argc, cmd->argv, last_argv_i_option_n + 1);
+	if (!last_argv_i_option_n)
 		printf("\n");
 	return (EXIT_SUCCESS);
 }
 
-static bool	get_option_n(int argc, char **argv)
+static int	get_last_argv_i_option_n(char **argv)
 {
-	int	size;
+	int	i;
+	int	j;
 
-	if (argc > 1)
+	i = 1;
+	while (argv[i])
 	{
-		size = ft_strlen(argv[1]);
-		if (size == 2 && !ft_strncmp(argv[1], "-n", size))
-			return (true);
+		if (argv[i][0] != '-')
+			return (i - 1);
+		j = 1;
+		while (argv[i][j])
+		{
+			if (argv[i][j] != 'n')
+				return (i - 1);
+			j++;
+		}
+		i++;
 	}
-	return (false);
+	return (i - 1);
 }
 
-static void	print_echo_argv(int argc, char **argv, bool option_n)
+static void	print_echo_argv(int argc, char **argv, int argv_i)
 {
-	int		i;
-	char	*newline;
-
-	if (option_n)
-		i = 2;
-	else
-		i = 1;
-	newline = NULL;
-	while (i < argc)
+	while (argv_i < argc)
 	{
-		if (option_n)
-			newline = ft_strchr(argv[i], '\n');
-		if (newline)
-		{
-			printf("%.*s", (int)(newline - argv[i]), argv[i]);
-			break ;
-		}
-		else
-			printf("%s", argv[i]);
-		if (i + 1 < argc)
+		printf("%s", argv[argv_i]);
+		if (argv_i + 1 < argc)
 			printf(" ");
-		i++;
+		argv_i++;
 	}
 }
